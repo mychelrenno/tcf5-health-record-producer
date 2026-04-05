707f6105c44fd0a205624c18346641aced87a5af
Mychel Renno <mychelrenno@gmail.com>
2026-04-04 22:45:54 -0300

    feat - adicionado configuracao

---
+++ src/test/java/com/devrenno/tcf5/health/record/producer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrenno.tcf5.health.record.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
